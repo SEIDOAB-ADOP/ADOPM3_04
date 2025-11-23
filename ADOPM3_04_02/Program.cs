d9f9dfbfa1027805e5d25d3569d69659c1a7cfff
Martin <martin_lenart@icloud.com>
2025-11-23 09:36:43 +0100

    Initial commit - cleaned history from branch main

---
+++ ADOPM3_04_02/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ADOPM3_04_02
+{
+    class Program
+    {
+
+        static string NotAnEnumerable()
+        {
+            return "Hello";
+            return "World";			//We will never come here
+            return "Hello again";
+        }
+        
+		static IEnumerable<string> AnEnumerable()
+        {
+			yield return "Hello";
+			yield return "World";
+			yield return "Hello again";
+        }
+
+		
+		static IEnumerable<string> BogusLatin()
+		{
+			var latin = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt" +
+				" ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris" +
+				" nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse" +
+				" cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa" +
+				" qui officia deserunt mollit anim id est laborum.";
+			var words = latin.Split(' ');
+
+			var rnd = new Random();
+			for (int i = 0; i < 10; i++)
+			{
+				yield return words[rnd.Next(0, words.Length)] + " ";
+			}			
+		}
+	
+		static IEnumerable<int> Fibs(int fibCount)
+		{
+			for (int i = 0, prevFib = 1, curFib = 1; i < fibCount; i++)
+			{
+				yield return prevFib;
+				int newFib = prevFib + curFib;
+				prevFib = curFib;
+				curFib = newFib;
+			}
+		}
+		/*
+		static IEnumerable<int> EvenNumbersOnly(IEnumerable<int> sequence)
+		{
+			foreach (int x in sequence)
+				if ((x % 2) == 0)
+					yield return x;
+		}
+		*/
+		
+
+		static void Main(string[] args)
+        {
+			Console.WriteLine(NotAnEnumerable());
+			Console.WriteLine(NotAnEnumerable());
+			Console.WriteLine(NotAnEnumerable());
+
+            Console.WriteLine();
+            foreach (var item in AnEnumerable())
+            {
+                Console.WriteLine(item);
+            }
+
+			var list = AnEnumerable().ToList();
+
+			Console.WriteLine();
+            foreach (var item in BogusLatin())
+            {
+                Console.WriteLine(item);
+            }
+
+			System.Console.WriteLine("\nWith Linq");
+			foreach (var item in BogusLatin().OrderBy(x => x))
+			{
+				System.Console.WriteLine(item);
+			}
+
+
+
+			foreach (var item in Fibs(15))
+			{
+				Console.WriteLine(item);
+			}
+			
+
+            /*
+			foreach (var item in BogusLatin())
+            {
+                Console.WriteLine(item);
+            }
+
+			
+			foreach (int fib in Fibs(15))
+				Console.WriteLine(fib);
+
+			
+            Console.WriteLine();
+			foreach (int fib in EvenNumbersOnly(Fibs(15)))
+				Console.WriteLine(fib);
+			*/
+
+        }
+	}
+	//Exercise:
+	//1.	Go through the code with the debugger step by step and explore what happens. Particular around the yield return statment.
+	//		Explore variables as you debug. Discuss in the group so you understand the flow. Explain
+}
---
+++ ADOPM3_04_10/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ADOPM3_04_10
+{
+    class Program
+    {
+        class Car : IEquatable<Car>
+        {
+            public int Year { get; set; }
+            public string Make { get; set; }
+
+            public override string ToString() => $"Year: {Year}, Make: {Make}";
+
+            //IEquatable
+            public override int GetHashCode() => (Year, Make).GetHashCode();
+            public bool Equals(Car other) => (Year, Make) == (other.Year, other.Make);
+            public override bool Equals(object obj) => Equals(obj as Car);
+        }
+
+        static void Main(string[] args)
+        {
+            //List of cars
+            List<Car> listRegNr= new List<Car>();
+            listRegNr.Add(new Car { Year = 2022, Make = "Volvo" });
+            listRegNr.Add(new Car { Year = 2019, Make = "Suzuki" });
+            System.Console.WriteLine(listRegNr[0]);
+            Car myCar0 = listRegNr[0];
+            System.Console.WriteLine(myCar0.GetHashCode());
+            
+            //Dictionary of cars
+            Dictionary<string, Car> dicRegNr= new Dictionary<string, Car>();
+            dicRegNr.Add("NMN 854", new Car { Year = 2022, Make = "Volvo" });
+            dicRegNr.Add("YMY 789", new Car { Year = 2019, Make = "Suzuki" });
+            
+            System.Console.WriteLine("NMN 854".GetHashCode());
+
+            //Accessing the Dictionary
+            Console.WriteLine(dicRegNr["NMN 854"]);
+            Car myCar = dicRegNr["YMY 789"];
+            Console.WriteLine(myCar);
+
+            //Using Car as Key
+            Dictionary<Car, string> dicCarUri = new Dictionary<Car, string>();
+            dicCarUri[new Car { Year = 2019, Make = "Suzuki" }] = "Uri to webpages for suzuki";
+            dicCarUri[new Car { Year = 2022, Make = "Volvo" }] = "Uri to webpages for volvo";
+            System.Console.WriteLine(dicCarUri[myCar0]);
+            System.Console.WriteLine(dicCarUri[myCar]);
+
+
+            //Value in Dictionary can be of any type, also a List
+            Dictionary<string, List<string>> dicFavoriteBands = new Dictionary<string, List<string>>();
+
+            dicFavoriteBands.Add("AC/DC", new List<string>() { "Fly on the Wall", "TnT", "For those about to Rock" });
+            dicFavoriteBands.Add("Pink Floyd", new List<string>() { "Dark side of the moon", "The Wall", "Final Cut" });
+            dicFavoriteBands.Add("Bob Dylan", new List<string>() { "Infidels", "Slow Train Comming" });
+
+            Console.WriteLine();
+            var l = dicFavoriteBands["AC/DC"];
+            l.ForEach(item => Console.WriteLine(item));
+
+            //Dictionay Keys and Values can be accessed as collections
+            foreach (var performer in dicFavoriteBands.Keys)
+            {
+                Console.WriteLine($"\n{performer} albums:");
+                foreach (var album in dicFavoriteBands[performer])
+                {
+                    Console.WriteLine(album);
+                }
+            }
+
+            //Fast check if a Dictionary contains a key
+            Console.WriteLine(dicFavoriteBands.ContainsKey("Abba"));
+            var myAlbums = dicFavoriteBands.ToList();
+
+
+            //Keys can also be of a complex type, because the IEquatable are used
+            Dictionary<Car, List<string>> dicSpareParts = new Dictionary<Car, List<string>>();
+
+            dicSpareParts.Add(new Car { Year = 2022, Make = "Volvo" },
+                new List<string>(){ "Sparepart1 for a 2020 Volvo", "Sparepart2 for a 2020 Volvo"});
+
+            dicSpareParts.Add(new Car { Year = 2019, Make = "Suzuki" },
+                new List<string>() { "Sparepart1 for a 2019 Suzuki", "Sparepart2 for a 2019 Suzuki",
+                                     "Sparepart3 for a 2019 Suzuki", "Sparepart3 for a 2019 Suzuki"});
+
+            foreach (var car in dicSpareParts.Keys)
+            {
+                Console.WriteLine($"\nSpareparts for {car}:");
+                foreach (var parts in dicSpareParts[car])
+                {
+                    Console.WriteLine(parts);
+                }
+            }
+        }
+    }
+}
